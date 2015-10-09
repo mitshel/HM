e46e370e0e0cf6a1ae56f738828eee69aff8f39c
Dmitry Shelepnev <mitshel@mail.ru>
2015-10-09 15:47:20 +0300

    Fix deploy Script

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,4 +2,5 @@
 
 DEPLOY_DIR='/home/www/homemediacat/'
 sudo -u www cp -rf HM assets hm_auth photogal templates manage.py README.md $DEPLOY_DIR
+sudo -u www sed -i "s/DEBUG = True/DEBUG = False/g" $DEPLOY_DIR"HM/settings.py"
 
