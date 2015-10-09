a512f6ba0f8834a40932a6dc617a7c99fb104706
Dmitry Shelepnev <mitshel@mail.ru>
2015-10-09 16:45:01 +0300

    Add static/README.md

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DEPLOY_DIR='/home/www/homemediacat/'
-sudo -u www cp -rf HM assets hm_auth photogal templates manage.py README.md $DEPLOY_DIR
+sudo -u www cp -rf HM assets hm_auth photogal templates static manage.py README.md $DEPLOY_DIR
 sudo -u www sed -i "s/DEBUG = True/DEBUG = False/g" $DEPLOY_DIR"HM/settings.py"
 
