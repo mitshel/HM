56c31138311288a2ad19a01000beb0f77865cd19
Dmitry Shelepnev <mitshel@mail.ru>
2015-10-09 13:31:39 +0300

    Fix #5 deploy script

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,11 +2,13 @@
 
 DEPLOY_DIR='/home/www/homemediacat/'
 
-cp -rf ./ $DEPLOY_DIR"HM"
-cp -rf ./assets $DEPLOY_DIR"assets"
-cp -rf ./hm_auth $DEPLOY_DIR"hm_auth"
-cp -rf ./photogal $DEPLOY_DIR"photogal"
-cp -rf ./static $DEPLOY_DIR"static"
-cp -rf ./templates $DEPLOY_DIR"templates"
-cp -rf ./manage.py $DEPLOY_DIR"manage.py"
-cp -rf ./README.md $DEPLOY_DIR"README.md"
+cp -rf HM assets hm_auth photogal static templates manage.py README.md $DEPLOY_DIR
+
+rem cp -rf ./HM $DEPLOY_DIR"HM"
+rem cp -rf ./assets $DEPLOY_DIR"assets"
+rem cp -rf ./hm_auth $DEPLOY_DIR"hm_auth"
+rem cp -rf ./photogal $DEPLOY_DIR"photogal"
+rem cp -rf ./static $DEPLOY_DIR"static"
+rem cp -rf ./templates $DEPLOY_DIR"templates"
+rem cp -rf ./manage.py $DEPLOY_DIR"manage.py"
+rem cp -rf ./README.md $DEPLOY_DIR"README.md"
