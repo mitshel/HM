7ee05352f9164acebd2b8a174cc1106e0ec7a7fb
Dmitry Shelepnev <mitshel@mail.ru>
2015-10-09 13:20:41 +0300

    New deploy script

---
+++ deploy/install.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+DEPLOY_DIR = '/home/www/homemediacat/'
+
+cp -rf ./ $DEPLOY_DIR"HM"
+cp -rf ./assets $DEPLOY_DIR"assets"
+cp -rf ./hm_auth $DEPLOY_DIR"hm_auth"
+cp -rf ./photogal $DEPLOY_DIR"photogal"
+cp -rf ./static $DEPLOY_DIR"static"
+cp -rf ./templates $DEPLOY_DIR"templates"
+cp -rf ./manage.py $DEPLOY_DIR"manage.py"
+cp -rf ./README.md $DEPLOY_DIR"README.md"
